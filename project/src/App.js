9d63f25d6e2a8473015fc4eafcdf6190215d5831
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-19 18:22:43 +0530

    home page work

--- project/src/App.js
+++ project/src/App.js
@@ -3,15 +3,22 @@ import { Routes, Route, Link } from 'react-router-dom';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
 import Home from './components/Home';
+import './App.css';
+import logo from './logo.png';
+
 
 const App = () => {
     return (
         <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/signup">Sign Up</Link>
-                <Link to="/login">Login</Link>
+          <div class="main-container">
+          <img src={logo} href={Home} alt="medical"/>
+          <nav class="nav-container">
+                <Link to="/" class="link">Home</Link>
+                <Link to="/signup" class="link">Sign Up</Link>
+                <Link to="/login" class="link">Login</Link>
             </nav>
+          </div>
+            
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/signup" element={<SignupForm/>} />
--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+const myStyle={
+    textAlign:"center",
+    padding:"150px"
+}
+const buttonStyle={
+    backgroundColor:"blue",
+    color:"white",
+    padding:"10px",
+    borderRadius:"30px"
+}
+
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to the Medical Appointment Booking System</h1>
-            <p>Please sign up or log in to book an appointment.</p>
+        <div style={myStyle}>
+            <h1 style={{fontSize:"40px",fontWeight:"bold"}}>Your Health Our Responsibilty</h1>
+            <p>Welcome to the Medical Appointment Online App. Please sign up or log in to book an appointment.</p>
+            <button style={buttonStyle}>Get Started</button>
         </div>
     );
 };
