fc53b8050c9767348c5c7c6bfddc66e4c90ae23d
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-24 12:29:32 +0530

    node

--- project/src/components/NavBar.jsx
+++ project/src/components/NavBar.jsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
-function NavBar(){
-    return(
+import './NavBar.css';  // Import the CSS file
+
+function NavBar() {
+    return (
         <div>
             <nav className="nav-container">
-               <Link to="/">
-                    <img src={logo} alt="Logo" height="70px" width="70px"/>
+                <Link to="/">
+                    <img src={logo} alt="Logo" className="nav-logo" />
                 </Link>
-                <Link to="/" className="link">Home</Link>
-                <Link to="/" className="link">Appointment</Link>
-                <Link to="/" className="link">Health Blog</Link>
-                <Link to="/" className="link">Reviews</Link>
-                <Link to="/signup" className="link">Sign Up</Link>
-                <Link to="/login" className="link">Login</Link>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/appointment" className="nav-link">Appointment</Link>
+                <Link to="/health-blog" className="nav-link">Health Blog</Link>
+                <Link to="/reviews" className="nav-link">Reviews</Link>
+                <Link to="/signup" className="nav-link">Sign Up</Link>
+                <Link to="/login" className="nav-link">Login</Link>
             </nav>
         </div>
     );
 };
+
 export default NavBar;
