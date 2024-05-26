ab08e2f75b57edec510c0693ca08f3a257132596
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-26 12:08:36 +0530

    home page changes

--- project/src/components/NavBar.jsx
+++ project/src/components/NavBar.jsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
-import '../../style/NavBar.css';  // Import the CSS file
+import '../style/NavBar.css';  // Import the CSS file
 
 function NavBar() {
     return (
         <div>
             <nav className="nav-container">
-                <Link to="/">
-                    <img src={logo} alt="Logo" className="nav-logo" />
-                </Link>
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/appointment" className="nav-link">Appointment</Link>
-                <Link to="/health-blog" className="nav-link">Health Blog</Link>
-                <Link to="/reviews" className="nav-link">Reviews</Link>
-                <Link to="/signup" className="nav-link">Sign Up</Link>
-                <Link to="/login" className="nav-link">Login</Link>
+                <div className="nav-left">
+                    <Link to="/">
+                        <img src={logo} alt="Logo" className="nav-logo" />
+                    </Link>
+                </div>
+                <div className="nav-right">
+                    <Link to="/" className="nav-link">Home</Link>
+                    <Link to="/appointment" className="nav-link">Appointment</Link>
+                    <Link to="/health-blog" className="nav-link">Health Blog</Link>
+                    <Link to="/reviews" className="nav-link">Reviews</Link>
+                    <Link to="/signup" className="nav-link border">Sign Up</Link>
+                    <Link to="/login" className="nav-link border">Login</Link>
+                </div>
             </nav>
         </div>
     );
